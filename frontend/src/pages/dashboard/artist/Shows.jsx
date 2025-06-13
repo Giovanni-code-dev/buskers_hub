96e6501a401632180d62c383312603a1d8f86387
Giovanni Delle Lenti <giovanni.dellelenti@gmail.com>
2025-06-13 21:36:07 +0200

    cardFooter

--- frontend/src/pages/dashboard/artist/Shows.jsx
+++ frontend/src/pages/dashboard/artist/Shows.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
